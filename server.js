df5a304aef1344368b3775c47595637775bd0fc4
stevenckohr-pixel <stevenckohr@gmail.com>
2026-03-30 11:25:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,12 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 // --- CONFIGURATION DE SÉCURITÉ (CORS) ---
 // Liste des origines autorisées (SANS slash à la fin)
-const allowedOrigins = [
-    'https://stevenckohr-pixel.github.io',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500'
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // Autorise les requêtes sans origine (comme Postman) ou les origines de la liste
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            console.log("CORS Bloqué pour l'origine :", origin);
-            callback(new Error('Non autorisé par CORS'));
-        }
-    },
+    origin: [
+        'https://stevenckohr-pixel.github.io',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
